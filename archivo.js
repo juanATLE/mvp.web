70fbc85d7dd5b78fe1c56aacb34cb4e60d818fb6
juanATLE <juan.ataco.l@uni.pe>
2026-01-02 23:09:48 -0500

    Initial commit: MVP web structure

---
+++ archivo.js
@@ -0,0 +1,12 @@
+let contador = 0;
+const texto = document.getElementById("valor");
+const btnSumar = document.getElementById("sumar");
+const btnRestar = document.getElementById("restar");
+btnSumar.addEventListener("click", () => {
+    contador++;
+    texto.textContent = contador;
+});
+btnRestar.addEventListener("click", () => {
+    contador--;
+    texto.textContent = contador;
+});
